4f5e9916ac9ff52ed6d57f6c55c87067d79e2cd9
Max Randolph <azzmodious@gmail.com>
2025-08-23 11:35:05 -0400

    adding startup script

--- client/start.sh
+++ client/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
+#source ~/miniconda3/etc/profile.d/conda.sh
 
 # Run first script in env_one
 nohup bash -c 'conda activate flask-audio && python /path/to/audio_client_rasp.py' > audio_client.log 2>&1 &
